44b2927d7ddfadb6a5cf9817b2698e6f1803365b
Francisco VIllarreal <paco_vip95@hotmail.com>
2026-01-22 11:20:25 -0600

    feat: setup amplify

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -11,7 +11,12 @@ export const auth = defineAuth({
       google: {
         clientId: secret("GOOGLE_CLIENT_ID"),
         clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-      }
+      },
+      callbackUrls: [
+        'http://localhost:3000/profile',
+        'https://mywebsite.com/profile'
+      ],
+      logoutUrls: ['http://localhost:3000/', 'https://mywebsite.com'],
     }
   },
 });
